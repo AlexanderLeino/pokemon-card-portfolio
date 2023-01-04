a19f11fc4a86ae0a16b1ebd843b79fe9aa4c4aa9
Alexander Leino <Alexander.Leino@yahoo.com>
2023-01-03 20:23:05 -0500

    working on creating components. created spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
+import { Spinner } from './components/Spinner';
+import "../../node_modules/nes.css/css/nes.css"
 function App() {
   return (
     <>
     <div className='px-2'>
       <Button />
-      
+      <Input />
     </div>
     </>
 
--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,7 +1,14 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
+import { Flex, FlexColumn } from "./Flex/flex-column";
 export const Input = () => {
+
+  const [value, setValue] = useState('')
+  const [isValid, setIsValid] = useState(false)
+
+
   return (
-    <input className='focus-visible:outline-amber-600 focus-visible:outline-4 outline-none border-solid border-4 border-red-500 p-0 m-0 hover:border-none ' />
-  )
-}
+    <>
+      <input className=" max-w-fit focus-visible: border-solid border-2 border-slate-500 hover:border-orange-500 p-1 mt-4 outline-none focus-visible:outline-none focus-visible:border-orange-500 " />
+    </>
+  );
+};
