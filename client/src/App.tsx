603f988577c6cd4d2181fa9c1a032a62740ba05b
Alexander Leino <Alexander.Leino@yahoo.com>
2023-01-07 14:41:04 -0500

    removed amp files

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import './App.css';
+import {CreateAccount} from './components/Form/AccountCreation/createAccount'
+const App:FC = () => {
+  return (
+     <CreateAccount />
+  );
+}
+
+export default App;
